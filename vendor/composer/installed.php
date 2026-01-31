1e1d1514379760b80f63379d21d47111e4d2058e
Ryan Waterbury <ryan.waterbury@onedog.solutions>
2026-01-31 14:22:15 -0600

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'stellarwp/wpcomplete',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8f3fd6d4020044a39b05ed1bcc8e0f4a3a67a77e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'stellarwp/wpcomplete' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8f3fd6d4020044a39b05ed1bcc8e0f4a3a67a77e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
