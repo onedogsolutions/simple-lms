b3fe8874bbf364eaaa679fabe1e8cfa9910efd9f
Ryan Waterbury <ryan.waterbury@onedog.solutions>
2026-02-28 19:06:44 -0600

    Updated stubs file for local development.

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bb742a54dd8020824d24');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c845c839abfbf9e9c0f7');
