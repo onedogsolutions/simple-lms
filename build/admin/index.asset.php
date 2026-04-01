0651447556219de34c1c3feffcb6924559d7b4f1
Ryan Waterbury <ryan.waterbury@onedog.solutions>
2026-03-31 23:41:16 -0500

    feat: add reset functionality to Phase 2 registration sync in admin migration UI

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c845c839abfbf9e9c0f7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd256a6aebd2d7624b46d');
