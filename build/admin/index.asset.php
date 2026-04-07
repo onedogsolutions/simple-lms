eeadf551523878d4d24260c491b670a042f39346
Ryan Waterbury <ryan.waterbury@onedog.solutions>
2026-04-07 13:20:18 -0500

    build: update asset version for admin index script

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd25c8a01751d9f3cfa16');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5356c3b4de35811840ff');
