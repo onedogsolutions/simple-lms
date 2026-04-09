0b2a61052dc9537e9dbe86ed45d9bfdc7639f240
Ryan Waterbury <ryan.waterbury@onedog.solutions>
2026-04-09 10:50:57 -0500

    refactor: update build assets and minified index script

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b818f61ddaf760857434');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3c74b0d9ebadd86b6e45');
