0c72d7d37c5205b3ac2ec0a525bebe83bc64f32a
Ryan Waterbury <ryan.waterbury@onedog.solutions>
2026-04-08 23:45:06 -0500

    chore: update asset version for admin build

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5356c3b4de35811840ff');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fec93a562da391433423');
